42b546d20af37ebae56829ad9c3f659400a0eff6
Robert Katzki <robert@katzki.de>
2022-01-16 17:54:16 +0100

    feat: parse data correctly

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,10 +1,9 @@
-import https from 'https';
 import type { NextRequest } from 'next/server'
 import { isToday, isTomorrow } from 'date-fns';
 
 function getCurrent(currently: any): string {
   const { summary, temperature } = currently;
-  return `Aktuell ${summary} bei ${Math.round(temperature)} Grad.`;
+  return `Aktuell ${summary} bei ${Math.round(temperature)} Grad. `;
 }
 
 function getForecast(hourly: any): string {
@@ -13,19 +12,9 @@ function getForecast(hourly: any): string {
   const temperatures = hourly.data
     .filter((hourData: any) => isDesiredDay(new Date(hourData.time * 1000)))
     .map((hourData: any) => hourData.temperature);
-  const maxTemperature = Math.max(temperatures);
+  const maxTemperature = Math.max(...temperatures);
 
-  return `Vorschau: ${hourly.summary} Maximal ${Math.round(maxTemperature)} Grad.`;
-}
-
-function fetch(url: string): Promise<any> {
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      let data = '';
-      res.on('data', (chunk) => data += chunk);
-      res.on('end', () => resolve(JSON.parse(data)));
-    }).on('error', reject);
-  });
+  return `Vorschau: ${hourly.summary} Maximal ${Math.round(maxTemperature)} Grad. `;
 }
 
 export async function middleware(req: NextRequest) {
@@ -33,9 +22,10 @@ export async function middleware(req: NextRequest) {
   const url = `https://api.darksky.net/forecast/${apiKey}/53.5630217,10.0095132?lang=de&units=si&exclude=daily,flags,alerts,minutely`;
 
   try {
-    const data = await fetch(url);
-    // const text = getCurrent(data.currently) + getForecast(data.hourly);
-    return new Response(data, { status: 200 });
+    const response = await fetch(url);
+    const data = await response.json();
+    const text = getCurrent(data.currently) + getForecast(data.hourly);
+    return new Response(text, { status: 200 });
   } catch (error) {
     console.log(error);
     return new Response(JSON.stringify({ error: 'Failed fetching data' }), {
